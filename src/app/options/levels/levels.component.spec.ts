a765933d0177eb035280235126be0ad46bff87b3
Fred Morel <github@fmorel.net>
2019-01-05 15:25:33 -0500

    Update testing infrastructure, fix remaining test

--- src/app/options/levels/levels.component.spec.ts
+++ src/app/options/levels/levels.component.spec.ts
@@ -3,11 +3,7 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LevelNamePipe } from '../../pipes/levelname.pipe';
-import { LevelStarsPipe } from '../../pipes/levelstars.pipe';
 import { LevelOptionsComponent } from './levels.component';
-
-let levelStarsStub: Partial<LevelStarsPipe>;
-levelStarsStub = { transform: (level) => ['★', '★★', '★★★'][level] };
 
 let levelNamesStub: Partial<LevelNamePipe>;
 levelNamesStub = { transform: (level) => ['Green', 'Yellow', 'Red'][level] };
@@ -18,9 +14,8 @@ describe('LevelOptionsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LevelOptionsComponent, LevelStarsPipe, LevelNamePipe],
+      declarations: [LevelOptionsComponent, LevelNamePipe],
       providers: [
-        { provide: LevelStarsPipe, useValue: levelStarsStub },
         { provide: LevelNamePipe, useValue: levelNamesStub },
       ],
       schemas: [NO_ERRORS_SCHEMA],
--- src/app/options/levels/levels.component.ts
+++ src/app/options/levels/levels.component.ts
@@ -6,6 +6,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class LevelOptionsComponent {
   @Output() public change = new EventEmitter<boolean[]>();
-  @Input() public levels: boolean[];
+  @Input() public levels: boolean[] = [];
   public select() { this.change.emit(this.levels.slice(0)); }
 }
