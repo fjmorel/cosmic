f270bcaee7f6a9bd97470b5ab9c26262d0de1fed
Fred Morel <github@fmorel.net>
2021-01-30 23:05:23 -0500

    Update angular material

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -15,7 +15,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes, { useHash: true, relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule { }
