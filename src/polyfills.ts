7648e9922874b1de175e0d58a23496858ac0219a
Fred Morel <github@fmorel.net>
2022-08-03 19:20:39 -0400

    Fill in Odyssey details
    Co-authored by: Quinlan <quinlan@zmulls.com>

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -20,29 +20,6 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. */
-// import 'core-js/es/symbol';
-// import 'core-js/es/object';
-// import 'core-js/es/function';
-// import 'core-js/es/parse-int';
-// import 'core-js/es/parse-float';
-// import 'core-js/es/number';
-// import 'core-js/es/math';
-// import 'core-js/es/string';
-// import 'core-js/es/date';
-// import 'core-js/es/array';
-// import 'core-js/es/regexp';
-// import 'core-js/es/map';
-// import 'core-js/es/weak-map';
-// import 'core-js/es/set';
-
-/** IE10 and IE11 requires the following for the Reflect API. */
-// import 'core-js/es/reflect';
-
-/** Evergreen browsers require these. */
-// Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-// import 'core-js/es7/reflect';
-
 /**
  * By default, zone.js will patch all possible macroTask and DomEvents
  * user can disable parts of macroTask/DomEvents patch by setting following flags
